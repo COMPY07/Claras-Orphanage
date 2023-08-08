6b92f1af8345310c24a0666b2f20cf562f278550
JeongHun Lee <56685470+COMPY07@users.noreply.github.com>
2023-08-08 19:50:08 +0900

    HeartBeat Sound

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -46,38 +46,21 @@ public class PlayerController : MonoBehaviour
     {
         if (isLive)
         {
-            // Heart()
+            Heart();
             //move player
-            float xInput = Input.GetAxisRaw("Horizontal");
-            float xSpeed = xInput * moveSpeed;
-
-            if (xInput != 0)
-            {
-                isWalk = true;
-                transform.localScale = new Vector3(xInput * 3, 3, 0);
-                animator.SetBool("isWalking", true);
-            }
-            else
-            {
-                animator.SetBool("isWalking", false);
-            }
-
-            if (!isHide)
-            {
-                rigid.velocity = new Vector2(xSpeed, 0.0f);
-            }
-
+            move(Input.GetAxisRaw("Horizontal"));
+            
             //hide player
             if (canHide && Input.GetKey(KeyCode.S)) {
                 rigid.velocity = new Vector2(0.0f, 0.0f);
                 isHide = true;
-                hidePanel.SetActive(true);
+                // hidePanel.SetActive(true);
                 animator.SetBool("isSiting", true);
             }
             else
             {
-                isHide = false;
-                hidePanel.SetActive(false);
+                isHide = false; 
+                // hidePanel.SetActive(false);
                 animator.SetBool("isSiting", false);
             }
 
@@ -90,6 +73,21 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void move(float xInput) {
+        if (xInput != 0)
+        {
+            isWalk = true;
+            transform.localScale = new Vector3(xInput * 3, 3, 0);
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+        if (!isHide) {
+            rigid.velocity = new Vector2(xInput * moveSpeed, 0.0f);
+        }
+    }
     void Heart()
     {
         if (audioSoure != null)
