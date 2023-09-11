3f02b10a13c94063a4626574d84088969c208fce
fluent0602 <129926172+fluent0602@users.noreply.github.com>
2023-09-12 01:30:10 +0900

    Map

---
+++ Assets/Scripts/Item/Slot.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour {
+    [SerializeField] private ItemInfo item;
+    private Image image;
+
+    private void Start() {
+        Debug.Log("초기화");
+        image = this.GetComponent<Image>();
+    }
+
+    public void SetItem(ItemInfo item) {
+        this.item = item;
+        image.sprite = item.GetImage();
+    }
+
+    public ItemInfo GetItem() {
+        return item;
+    }
+
+    public void OnButtonClick() {
+        if (item == null) return;
+
+        // type 
+        // 1 puzzle
+        // 2 ..
+        // 3 ...
+        item.Use();
+    }
+    
+    
+}
